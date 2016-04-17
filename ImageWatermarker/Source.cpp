d21091524713896713eb15b4ab116ff860f4ab6c
Allaoua <allaouabenchikh@gmail.com>
2016-04-17 14:32:16 +0100

    Implemented Embedding and extraction Request are now JSON compatible

--- ImageWatermarker/Source.cpp
+++ ImageWatermarker/Source.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include "json\json.h"
 
 using namespace cv;
 using namespace std;
@@ -10,6 +11,8 @@ namespace watermark {
 
 	int embedWatermark(std::vector<std::string> imagesPath, string watermarkPath, float alpha) {
 
+		Json::Value result;
+
 		/* ----------------   Loading images  ---------------------- */
 		int imgNumber = imagesPath.size();
 		std::vector<Mat> originalImages;
@@ -17,7 +20,8 @@ namespace watermark {
 		while (i < imgNumber) {
 			Mat imageread = imread(imagesPath.at(i), IMREAD_COLOR);
 			if (!imageread.data) {
-				cout << "Could not open or find the image at : " << imagesPath.at(i) << std::endl;
+				result["error"]["message"] = "Could not open or find the image at : " + imagesPath.at(i);
+				std::cerr << result << std::endl;
 				return -1;
 			}
 			originalImages.push_back(imageread);
@@ -26,14 +30,14 @@ namespace watermark {
 
 		Mat watermark = imread(watermarkPath, IMREAD_GRAYSCALE);
 		if (!watermark.data) {
-			cout << "Could not open or find the watermark image at : " << watermarkPath << std::endl;
+			result["error"]["message"] = "Could not open or find the watermark image at : " + watermarkPath;
+			std::cerr << result << std::endl;
 			return -1;
 		}
 		/* ------------------------------- Begining watermark embedding----------------------------- */
 
 		// Preprocessing watermark
 		// Applying Treshold
-		cout << "Preprocessing watermark" << endl;
 		threshold(watermark, watermark, 128, 255, THRESH_BINARY);
 
 		// Converting Watermark image into array
@@ -48,10 +52,11 @@ namespace watermark {
 		}
 		// Splitting image into channels
 		imgNumber = originalImages.size();
-		cout << "Number of images : " << imgNumber << endl;
-		
+		// the number of images that have been watermarked;
+		int watermarkedNumber = 0;
+		// Used to calculate the redundancy of the inserted watermark
 		int redundancy = 0;
-
+		// Represents the index of the image to be watermarked
 		i = 0;
 		// j represents index of current watermark pixel;
 		int j = 0;
@@ -70,82 +75,78 @@ namespace watermark {
 			int x = 0;
 			// y represents collumn index in the original image;;
 			int y = 0;
-			
-			Mat dctMat;
+			// RGB 8x8 Blocs of the image
 			Mat blocB;
 			Mat blocG;
 			Mat blocR;
-			Scalar intensity1;
-			Scalar intensity2;
+			// Used to store the intensity of the DCT Matrix
+			Scalar intensity;
+			// Used to store the intensity of the watermark pixel.
 			Scalar watermarkIntensity;
 			// Reading 8x8 bloc from image and inserting the watermark
 			while (x + 8 <= channels[2].rows) {
 				y = 0;
 				while (y + 8 <= channels[2].cols) {
-					// Reading 8x8 bloc;
+					// Reading 8x8 blocs;
 					blocB = Mat(channels[0], Rect(y, x, 8, 8));
 					blocG = Mat(channels[1], Rect(y, x, 8, 8));
 					blocR = Mat(channels[2], Rect(y, x, 8, 8));
-					// Processing DCT of the bloc;
+					// Processing DCT of the blocs;
 					dct(blocB, blocB);
 					dct(blocG, blocG);
 					dct(blocR, blocR);
 					// Reading the watermark intensity at position j;
 					watermarkIntensity = watermarkArray.at(j);
-					intensity1 = blocB.at<float>(5, 2);
-					intensity2 = blocB.at<float>(4, 3);
-					//cout << "8x8 : " << blocB.at<float>(5, 5) << endl;
-					// Watermark insertion;
-					
-					if (watermarkIntensity.val[0] == 0) {
 
+					// Watermark insertion;
+					if (watermarkIntensity.val[0] == 0) {
 						blocB.at<float>(5, 5) = alpha;
 						blocG.at<float>(5, 5) = alpha;
 						blocR.at<float>(5, 5) = alpha;
-
-						
-						/*if (intensity1.val[0] < intensity2.val[0]) {
-					
-						
-							blocB.at<float>(5, 2) = 20;
-							blocB.at<float>(4, 3) = -20;
-							blocG.at<float>(5, 2) = 20;
-							blocG.at<float>(4, 3) = -20;
-							blocR.at<float>(5, 2) = 20;
-							blocR.at<float>(4, 3) = -20;
-						}*/
 					}
 					else {
-						
 						blocB.at<float>(5, 5) = -alpha;
 						blocG.at<float>(5, 5) = -alpha;
 						blocR.at<float>(5, 5) = -alpha;
-						/*
-						if (intensity1.val[0] >= intensity2.val[0]) {
-							
-							blocB.at<float>(5, 2) = -20;
-							blocB.at<float>(4, 3) = 20;
-							blocG.at<float>(5, 2) = -20;
-							blocG.at<float>(4, 3) = 20;
-							blocR.at<float>(5, 2) = -20;
-							blocR.at<float>(4, 3) = 20;
-						}*/
 					}
-					//cout << "8x8 : " << blocB.at<float>(5, 5) << endl;
-					//cout << bloc.at<float>(5, 2) << endl;
-					//cout << bloc.at<float>(4, 3) << endl;
-					// Transforming the bloc back into spatial domain;
-					dct(blocB, blocB, DCT_INVERSE);
-					dct(blocG, blocG, DCT_INVERSE);
-					dct(blocR, blocR, DCT_INVERSE);
 					// Go to the next watermark pixel;
 					j = j + 1;
 					// if it is the last pixel then circle back to the begining;
 					if (j == watermarkArray.size()) {
 						j = 0;
 						redundancy = redundancy + 1;
-						cout << "Redundancy : " << redundancy << '\n';
+						watermarkedNumber = i + 1;
 					}
+
+					watermarkIntensity = watermarkArray.at(j);
+					if (watermarkIntensity.val[0] == 0) {
+
+						blocB.at<float>(4, 4) = alpha;
+						blocG.at<float>(4, 4) = alpha;
+						blocR.at<float>(4, 4) = alpha;
+					}
+					else {
+
+						blocB.at<float>(4, 4) = -alpha;
+						blocG.at<float>(4, 4) = -alpha;
+						blocR.at<float>(4, 4) = -alpha;
+
+					}
+
+					// Transforming the bloc back into spatial domain;
+					dct(blocB, blocB, DCT_INVERSE);
+					dct(blocG, blocG, DCT_INVERSE);
+					dct(blocR, blocR, DCT_INVERSE);
+
+					// Go to the next watermark pixel;
+					j = j + 1;
+					// if it is the last pixel then circle back to the begining;
+					if (j == watermarkArray.size()) {
+						j = 0;
+						redundancy = redundancy + 1;
+						watermarkedNumber = i + 1;
+					}
+
 					// Next Bloc
 					y = y + 8;
 				}
@@ -158,16 +159,13 @@ namespace watermark {
 			channels[1].convertTo(channels[1], CV_8UC1, 1.0);
 			channels[2].convertTo(channels[2], CV_8UC1, 1.0);
 			merge(channels, 3, image);
-			
-			
 			originalImages.at(i) = image;
-			cout << "Row : " << x << endl;
-			cout << "Col : " << y << endl;
 			// Go to the next image
 			i = i + 1;
 		}
 		if (redundancy == 0) {
-			cout << "Rendundancy is null: Watermark insertion failed due to : insufficient image dimensions" << std::endl;
+			result["error"]["message"] = "Watermark insertion failed due to : insufficient images size";
+			std::cerr << result << std::endl;
 			return -1;
 		}
 
@@ -175,82 +173,30 @@ namespace watermark {
 		//imshow("Display window", originalImages.at(0)); // Show our image inside it.
 		//waitKey(0); // Wait for a keystroke in the window
 		// Write extracted watermarks
-		for (int i = 0; i < originalImages.size(); ++i) {
-			std::string name = std::string("watermarked" + std::to_string(i));
-			std::string ext = std::string(".jpg");
-			imwrite(name + ext, originalImages.at(i));
+		for (int i = 0; i < watermarkedNumber; ++i) {
+			// this will errase the input image and save the watermarked one.
+			result["watermarkedImages"][i] = imagesPath.at(i);
+			imwrite(imagesPath.at(i), originalImages.at(i));
 		}
+
+		result["success"] = true;
+		result["watermarkedNumber"] = watermarkedNumber;
+		result["watermarkRedundancy"] = redundancy;
+		result["watermarkWidth"] = watermark.cols;
+		result["watermarkHeigth"] = watermark.rows;
+
+		std::cout << result << std::endl;
 
 		return 0;
-		/*
-		cout << "Number of channels : " << image.channels() << std::endl;
-		Mat channels[3];
-		split(image, channels);
-		// Converting blue channel of image to float
-		channels[2].convertTo(channels[2], CV_32F, 1.0 / 255);
-
-
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		int cpt = 0;
-		Mat dctMat;
-		Mat bloc;
-		Scalar intensity1;
-		Scalar intensity2;
-		Scalar watermarkIntensity;
-
-		while (i + 8 <= channels[2].rows && k < watermarkArray.size()) {
-			j = 0;
-			while (j + 8 <= channels[2].cols && k < watermarkArray.size()) {
-				bloc = Mat(channels[2], Rect(j, i, 8, 8));
-				dct(bloc, bloc);
-
-				watermarkIntensity = watermarkArray.at(k);
-				intensity1 = bloc.at<float>(5, 2);
-				intensity2 = bloc.at<float>(4, 3);
-				if (watermarkIntensity.val[0] == 0) {
-					if (intensity1.val[0] < intensity2.val[0]) {
-
-						float temp;
-						temp = bloc.at<float>(5, 2);
-						bloc.at<float>(5, 2) = bloc.at<float>(4, 3);
-						bloc.at<float>(4, 3) = temp;
-					}
-				}
-				else {
-					if (intensity1.val[0] > intensity2.val[0]) {
-
-						float temp;
-						temp = bloc.at<float>(5, 2);
-						bloc.at<float>(5, 2) = bloc.at<float>(4, 3);
-						bloc.at<float>(4, 3) = temp;
-					}
-				}
-
-				dct(bloc, bloc, DCT_INVERSE);
-
-				k = k + 1;
-				j = j + 8;
-			}
-
-			i = i + 8;
-		}
-		channels[2].convertTo(channels[2], CV_8U, 255 / 1.0);
-		cout << "number of pixels inserted : " << k << std::endl;
-
-
-		namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-		imshow("Display window", channels[2]); // Show our image inside it.
-		*/
 	};
 
 	int extractWatermark(std::vector<std::string> imagesPath, int width, int heigth) {
 		
+		Json::Value result;
 		// extracted watermarks
 		std::vector<Mat> watermarks;
 		// Variable that stores an extracted watermark;
-		Mat watermark = Mat(width, heigth, CV_8U, Scalar(0));
+		Mat watermark = Mat(width, heigth, CV_8UC1, Scalar(0));
 		watermark.at<uchar>(0, 0) = 0;
 		int watermarkSize = width * heigth;
 		
@@ -261,7 +207,8 @@ namespace watermark {
 		while (i < imgNumber) {
 			Mat imageread = imread(imagesPath.at(i), IMREAD_COLOR);
 			if (!imageread.data) {
-				cout << "Could not open or find the image at : " << imagesPath.at(i) << std::endl;
+				result["error"]["message"] = "Could not open or find the image at : " + imagesPath.at(i);
+				std::cerr << result << std::endl;
 				return -1;
 			}
 			originalImages.push_back(imageread);
@@ -288,12 +235,11 @@ namespace watermark {
 
 			// x represents row index in the original image;
 			int x = 0;
-			// y represents collumn index in the original image;;
+			// y represents collumn index in the original image;
 			int y = 0;
 			
 			Mat bloc;
-			Scalar intensity1;
-			Scalar intensity2;
+			Scalar intensity;
 			
 			// Reading 8x8 bloc from image and extracting the watermark
 			while (x + 8 <= channels[0].rows) {
@@ -304,22 +250,16 @@ namespace watermark {
 					// Processing DCT of the bloc;
 					dct(bloc, bloc);
 					
-					// Reading the watermark intensity at position j;
-					intensity1 = bloc.at<float>(5, 5);
-					intensity2 = bloc.at<float>(4, 3);
-					//cout << bloc.at<float>(5, 2) << endl;
-					//cout << bloc.at<float>(4, 3) << endl;
+					// Reading the bloc intensity at position (5,5);
+					intensity = bloc.at<float>(5, 5);
+
 					// Watermark extraction;
-					
-					cout << intensity1.val[0] << endl;
-					if (intensity1.val[0] > 0) {
+					if (intensity.val[0] > 0) {
 						watermark.at<uchar>(k, j) = 0;
 					}
 					else {
 						watermark.at<uchar>(k, j) = 255;
 					}
-					
-
 					// Go to the next watermark pixel;
 					j = j + 1;
 					if (j == watermark.cols) {
@@ -331,7 +271,30 @@ namespace watermark {
 							Mat extractedWatermark = Mat(width, heigth, CV_8U, 0);
 							watermark.copyTo(extractedWatermark);
 							watermarks.push_back(extractedWatermark);
-							cout << "Redundancy : " << redundancy << '\n';
+						}
+					}
+
+					// Reading the watermark intensity at position (4, 4);
+					intensity = bloc.at<float>(4, 4);
+
+					// Watermark extraction;
+					if (intensity.val[0] > 0) {
+						watermark.at<uchar>(k, j) = 0;
+					}
+					else {
+						watermark.at<uchar>(k, j) = 255;
+					}
+
+					j = j + 1;
+					if (j == watermark.cols) {
+						j = 0;
+						k = k + 1;
+						if (k == watermark.rows) {
+							k = 0;
+							redundancy = redundancy + 1;
+							Mat extractedWatermark = Mat(width, heigth, CV_8UC1, 0);
+							watermark.copyTo(extractedWatermark);
+							watermarks.push_back(extractedWatermark);
 						}
 					}
 					
@@ -344,16 +307,25 @@ namespace watermark {
 			// Next Image
 			i = i + 1;
 		}
+		if (redundancy == 0) {
+			result["error"]["message"] = "Watermark insertion failed due to : insufficient images size";
+			std::cerr << result << std::endl;
+			return -1;
+		}
 		// Write extracted watermarks
 		for (int i = 0; i < watermarks.size(); ++i) {
 			std::string name = std::string("watermark" + std::to_string(i));
-			std::string ext = std::string(".jpg");
+			std::string ext = std::string(".png");
+			std::string path = std::string(name + ext);
+			result["watermarks"][i] = path;
 			imwrite(name + ext, watermarks.at(i));
 		}
+		result["success"] = true;
 		/*namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
 		imshow("Display window", watermarks.at(0)); // Show our image inside it.
 		waitKey(0); // Wait for a keystroke in the window*/
 		
+		std::cout << result << std::endl;
 
 		return 0;
 	}
@@ -361,89 +333,170 @@ namespace watermark {
 
 int main(int argc, char** argv)
 {
-	if (argc <= 1)
-	{
-		cout << "Usage: [Image] [Image]* ( -watermark [Image] || -extract )" << endl;
-		return -1;
-	}
+	
+	if (argc == 1) {
+		// Reading JSON data:
+		Json::Value root;
+		// JSON for error output
+		Json::Value error;
 
-	int i = 1;
-	std::string param = std::string(argv[i]);
-	std::vector<std::string> imagesPath;
-	while (i < argc) {
-		param = std::string(argv[i]);
-		if (param == "-watermark" || param == "-extract") {
-			break;
-		}
-		imagesPath.push_back(param);
-		i = i + 1;
-	}
+		std::string str;
+		// Reading JSON string from stdin;
+		std::getline(std::cin, str);
+		// Parsing the JSON string;
+		Json::Reader reader;
+		if (reader.parse(str, root)) {
+			if (!root.isMember("method")) {
+				error["error"]["message"] = "Please specify the method";
+				std::cerr << error << std::endl;
+				return -1;
+			}
 
-	if (imagesPath.size() == 0) {
-		cout << " Specify Images to watermark" << endl;
-		return -1;
-	} 
+			if (root["method"].asString() != "watermark" && root["method"].asString() != "extract") {
+				error["error"]["message"] = "Unknown method : " + root["method"].asString();
+				std::cerr << error << std::endl;
+				return -1;
+			}
 
-	if (i >= argc) {
-		cout << "Specify weither you want to insert or extract a watermark : use the options -watermark or -extract" << endl;
-		return -1;
-	}
+			std::vector<std::string> imagesPath;
 
-	if (param == "-watermark") {
-		if (i + 1 >= argc) {
-			cout << "Specify the watermark image path";
-			return -1;
-		}
-		i = i + 1;
-		std::string watermarkPath = std::string(argv[i]);
-		float alpha = 50;
-
-		i = i + 1;
-		if (i + 1 < argc) {
-			param = std::string(argv[i]);
-			if (param == "-alpha") {
-				i = i + 1;
-				std::string alphaStr = std::string(argv[i]);
-				try {
-					alpha = std::stoi(alphaStr);
+			if (root.isMember("images") && root["images"].isArray()) {
+				for (int i = 0; i < root["images"].size(); ++i) {
+					imagesPath.push_back(root["images"][i].asString());
 				}
-				catch (const std::exception& e) {
-					cout << "alpha must be an Integer" << std::endl;
+			}
+			else {
+				error["error"]["message"] = "Specify Images to watermark.";
+				std::cerr << error << std::endl;
+				return -1;
+			}
+
+			if (root["method"].asString() == "watermark") {
+				if (!root.isMember("watermark")) {
+					error["error"]["message"] = "Specify the watermark image.";
+					std::cerr << error << std::endl;
 					return -1;
 				}
+				std::string watermarkPath = root["watermark"].asString();
+				if (!root.isMember("alpha")) {
+					error["error"]["message"] = "Specify the strength factor (alpha)";
+					std::cerr << error << std::endl;
+					return -1;
+				}
+				float alpha = root["alpha"].asFloat();
+				
+				// Embedding watermark
+				return watermark::embedWatermark(imagesPath, watermarkPath, alpha);
+			}
+			else if (root["method"].asString() == "extract") {
+				if (!root.isMember("width")) {
+					error["error"]["message"] = "Specify the watermark image width.";
+					std::cerr << error << std::endl;
+					return -1;
+				}
+				if (!root.isMember("heigth")) {
+					error["error"]["message"] = "Specify the watermark image heigth.";
+					std::cerr << error << std::endl;
+					return -1;
+				}
+				int width = root["width"].asInt();
+				int heigth = root["heigth"].asInt();
+				
+				// Extracting watermark
+				return watermark::extractWatermark(imagesPath, width, heigth);
+			}
+		}
+		else {
+			error["error"]["message"] = "Failed to parse JSON";
+			std::cerr << error << std::endl;
+			return -1;
+		}
+	}
+	// Parsing CMD line params
+	else {
+
+		int i = 1;
+		std::string param = std::string(argv[i]);
+		std::vector<std::string> imagesPath;
+		while (i < argc) {
+			param = std::string(argv[i]);
+			if (param == "-watermark" || param == "-extract") {
+				break;
+			}
+			imagesPath.push_back(param);
+			i = i + 1;
+		}
+
+		if (imagesPath.size() == 0) {
+			std::cerr << " Specify Images to watermark" << std::endl;
+			return -1;
+		}
+
+		if (i >= argc) {
+			std::cerr << "Specify weither you want to insert or extract a watermark : use the options -watermark or -extract" << std::endl;
+			return -1;
+		}
+
+		if (param == "-watermark") {
+			if (i + 1 >= argc) {
+				std::cerr << "Specify the watermark image path";
+				return -1;
+			}
+			i = i + 1;
+			std::string watermarkPath = std::string(argv[i]);
+			float alpha = 50;
+
+			i = i + 1;
+			if (i + 1 < argc) {
+				param = std::string(argv[i]);
+				if (param == "-alpha") {
+					i = i + 1;
+					std::string alphaStr = std::string(argv[i]);
+					try {
+						alpha = std::stoi(alphaStr);
+					}
+					catch (const std::exception& e) {
+						std::cerr << "alpha must be an Integer" << std::endl;
+						return -1;
+					}
+				}
+			}
+
+			return watermark::embedWatermark(imagesPath, watermarkPath, alpha);
+		}
+		else {
+			if (param == "-extract") {
+				i = i + 1;
+				if (i >= argc) {
+					std::cerr << "Specify watermark image width" << std::endl;
+					return -1;
+				}
+				std::string widthString = std::string(argv[i]);
+				i = i + 1;
+				if (i >= argc) {
+					std::cerr << "Specify watermark image heigth" << std::endl;
+					return -1;
+				}
+				std::string heigthString = std::string(argv[i]);
+				int width;
+				int heigth;
+				try {
+					width = std::stoi(widthString);
+					heigth = std::stoi(heigthString);
+				}
+				catch (const std::exception& e) {
+					std::cerr << "Width and Heigth must be Integers" << std::endl;
+					return -1;
+				}
+
+				return watermark::extractWatermark(imagesPath, width, heigth);
 			}
 		}
 
-		return watermark::embedWatermark(imagesPath, watermarkPath, alpha);
 	}
-	else {
-		if (param == "-extract") {
-			i = i + 1;
-			if (i >= argc) {
-				cout << "Specify watermark image width" << std::endl;
-				return -1;
-			}
-			std::string widthString = std::string(argv[i]);
-			i = i + 1;
-			if (i >= argc) {
-				cout << "Specify watermark image heigth" << std::endl;
-				return -1;
-			}
-			std::string heigthString = std::string(argv[i]);
-			int width;
-			int heigth;
-			try {
-				width = std::stoi(widthString);
-				heigth = std::stoi(heigthString);
-			}
-			catch (const std::exception& e) {
-				cout << "Width and Heigth must be Integers" << std::endl;
-				return -1;
-			}
-			
-			return watermark::extractWatermark(imagesPath, width, heigth);
-		}
-	}
+
+	return -1;
+	
 };
 
 
